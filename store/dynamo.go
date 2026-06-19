2a0d8aae17baae907cb5f3a02aac4f99ba17fa29
umang-sarda <sarda.u@northeastern.edu>
2026-06-19 16:20:39 -0400

    Adding package declarations to store files

---
+++ store/dynamo.go
@@ -0,0 +1,4 @@
+package store
+
+// DynamoDB client will be wired here in the next phase.
+// Currently using in-memory store via handlers package.
---
+++ store/redis.go
@@ -0,0 +1,4 @@
+package store
+
+// Redis client will be wired here in the next phase.
+// Currently using in-memory store via handlers package.
